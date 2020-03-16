380b772ec5af717dcb214b2665f94737257e4997
Jonathan Jogenfors <etnoy@broach.se>
2020-03-16 23:54:16 +0100

    Use maven tests

--- server/src/main/java/org/owasp/securityshepherd/SecurityShepherdApplication.java
+++ server/src/main/java/org/owasp/securityshepherd/SecurityShepherdApplication.java
@@ -1,17 +1,12 @@
 package org.owasp.securityshepherd;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
 import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
-import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
 
 import io.r2dbc.spi.ConnectionFactory;
 
@@ -20,14 +15,6 @@ public class SecurityShepherdApplication {
 
   public static void main(String[] args) throws Throwable {
     SpringApplication.run(SecurityShepherdApplication.class, args);
-  }
-
-  public R2dbcCustomConversions r2dbcCustomConversions() {
-
-    List<Converter<?, ?>> converterList = new ArrayList<Converter<?, ?>>();
-    converterList.add(new UserReadConverter());
-    return new R2dbcCustomConversions(converterList);
-
   }
 
   @Bean
