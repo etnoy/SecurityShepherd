6ea5b954b50e68a27aac06523c551141bbeab077
Jonathan Jogenfors <etnoy@broach.se>
2020-03-17 22:58:35 +0100

    Add it folder

---
+++ server/src/integration-test/java/org/owasp/securityshepherd/it/auth/UserIT.java
@@ -0,0 +1,28 @@
+package org.owasp.securityshepherd.it.auth;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.owasp.securityshepherd.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class UserIT {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void userAuth_HashedPassword_ReturnsHash() {
+
+
+		assertThat(false, is(true));
+	}
+
+}
