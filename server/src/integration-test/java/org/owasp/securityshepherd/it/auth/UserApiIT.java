deb44502f05a6ee4f63bb4e10676295198079e34
Jonathan Jogenfors <etnoy@broach.se>
2020-03-20 22:32:59 +0100

    Split off UserIT into two files

---
+++ server/src/integration-test/java/org/owasp/securityshepherd/it/auth/UserApiIT.java
@@ -0,0 +1,143 @@
+package org.owasp.securityshepherd.it.auth;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.owasp.securityshepherd.persistence.model.User;
+import org.owasp.securityshepherd.service.UserService;
+import org.owasp.securityshepherd.web.dto.PasswordUserRegistrationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Hooks;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class UserApiIT {
+
+  @Autowired
+  UserService userService;
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Test
+  public void apiGetUser_ValidId_ReturnsUser() throws Exception {
+
+    webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(
+            new PasswordUserRegistrationDto("TestUser1", "loginName1", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
+        .getResponseBody();
+
+    final User testUser = webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(new PasswordUserRegistrationDto("TestUserDisplayName",
+            "loginName", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
+        .getResponseBody();
+
+    webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(
+            new PasswordUserRegistrationDto("TestUser3", "loginName3", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
+        .getResponseBody();
+
+    FluxExchangeResult<User> getResult = webTestClient.get().uri("/api/v1/user/" + testUser.getId())
+        .accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
+        .contentType(MediaType.APPLICATION_JSON).returnResult(User.class);
+
+    StepVerifier.create(getResult.getResponseBody()).assertNext(getData -> {
+
+      assertThat(getData, is(testUser));
+
+    }).expectComplete().verify();
+
+  }
+  
+  @Test
+  public void apiListUsers_NoUsersExist_ReturnsNoUsers() throws Exception {
+
+    StepVerifier
+        .create(webTestClient.get().uri("/api/v1/user/list").accept(MediaType.APPLICATION_JSON)
+            .exchange().expectStatus().isOk().expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .returnResult(User.class).getResponseBody())
+        .expectComplete().verify();
+
+  }
+
+  @Test
+  public void apiListUsers_UsersExist_ReturnsUserList() throws Exception {
+
+    final User testUser1 = webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(new PasswordUserRegistrationDto("TestUserDisplayName",
+            "loginName", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
+        .getResponseBody();
+
+    final User testUser2 = webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(
+            new PasswordUserRegistrationDto("TestUser2", "loginName2", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
+        .getResponseBody();
+    
+    final User testUser3 = webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(
+            new PasswordUserRegistrationDto("TestUser3", "loginName3", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
+        .getResponseBody();
+
+    StepVerifier
+        .create(webTestClient.get().uri("/api/v1/user/list").accept(MediaType.APPLICATION_JSON)
+            .exchange().expectStatus().isOk().expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .returnResult(User.class).getResponseBody())
+        .expectNext(testUser1).expectNext(testUser2).expectNext(testUser3).expectComplete().verify();
+
+  }
+
+  @Test
+  public void apiUserCreate_ValidData_ReturnsValidUser() throws Exception {
+
+    webTestClient.post().uri("/api/v1/user/register/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(new PasswordUserRegistrationDto("TestUserDisplayName",
+            "loginName", "paLswOrdha17£@£sh")))
+        .exchange().expectStatus().isCreated();
+
+    FluxExchangeResult<String> getResult = webTestClient.get().uri("/api/v1/user/list")
+        .accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
+        .contentType(MediaType.APPLICATION_JSON).returnResult(String.class);
+
+    StepVerifier.create(getResult.getResponseBody()).assertNext(getData -> {
+
+      assertThat(getData, is(notNullValue()));
+
+    }).expectComplete().verify();
+
+  }
+
+  @BeforeEach
+  private void setUp() {
+    // Print more verbose errors if something goes wrong with reactor
+    Hooks.onOperatorDebug();
+
+    userService.deleteAll().block();
+  }
+
+}
--- server/src/integration-test/java/org/owasp/securityshepherd/it/auth/UserIT.java
+++ server/src/integration-test/java/org/owasp/securityshepherd/it/auth/UserIT.java
@@ -1,12 +1,10 @@
 package org.owasp.securityshepherd.it.auth;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,32 +12,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.owasp.securityshepherd.persistence.model.Auth;
 import org.owasp.securityshepherd.persistence.model.PasswordAuth;
 import org.owasp.securityshepherd.persistence.model.User;
-import org.owasp.securityshepherd.service.ClassService;
 import org.owasp.securityshepherd.service.UserService;
-import org.owasp.securityshepherd.web.controller.UserController;
-import org.owasp.securityshepherd.web.dto.PasswordUserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.EntityExchangeResult;
-import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Hooks;
 import reactor.test.StepVerifier;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -51,76 +32,6 @@ public class UserIT {
 
   @Autowired
   private WebTestClient webTestClient;
-
-
-  @Test
-  public void userGet_ValidId_ReturnsUser() throws Exception {
-
-    final User testUser = webTestClient.post().uri("/api/v1/user/register/password")
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(BodyInserters.fromValue(new PasswordUserRegistrationDto("TestUserDisplayName",
-            "loginName", "paLswOrdha17£@£sh")))
-        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
-        .getResponseBody();
-
-    FluxExchangeResult<User> getResult = webTestClient.get().uri("/api/v1/user/" + testUser.getId())
-        .accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
-        .contentType(MediaType.APPLICATION_JSON).returnResult(User.class);
-
-    StepVerifier.create(getResult.getResponseBody()).assertNext(getData -> {
-
-      assertThat(getData, is(testUser));
-
-    }).expectComplete().verify();
-
-  }
-
-  @Test
-  public void userList_ValidData_ReturnsUserList() throws Exception {
-
-    final User testUser1 = webTestClient.post().uri("/api/v1/user/register/password")
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(BodyInserters.fromValue(new PasswordUserRegistrationDto("TestUserDisplayName",
-            "loginName", "paLswOrdha17£@£sh")))
-        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
-        .getResponseBody();
-
-    final User testUser2 = webTestClient.post().uri("/api/v1/user/register/password")
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(BodyInserters.fromValue(
-            new PasswordUserRegistrationDto("TestUser2", "loginName3", "paLswOrdha17£@£sh")))
-        .exchange().expectStatus().isCreated().expectBody(User.class).returnResult()
-        .getResponseBody();
-
-    FluxExchangeResult<User> getResult = webTestClient.get().uri("/api/v1/user/list")
-        .accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
-        .contentType(MediaType.APPLICATION_JSON).returnResult(User.class);
-
-    StepVerifier.create(getResult.getResponseBody()).expectNext(testUser1).expectNext(testUser2)
-        .expectComplete().verify();
-
-  }
-
-  @Test
-  public void apiUserCreate_ValidData_ReturnsValidUser() throws Exception {
-
-    webTestClient.post().uri("/api/v1/user/register/password")
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(BodyInserters.fromValue(new PasswordUserRegistrationDto("TestUserDisplayName",
-            "loginName", "paLswOrdha17£@£sh")))
-        .exchange().expectStatus().isCreated();
-
-    FluxExchangeResult<String> getResult = webTestClient.get().uri("/api/v1/user/list")
-        .accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
-        .contentType(MediaType.APPLICATION_JSON).returnResult(String.class);
-
-    StepVerifier.create(getResult.getResponseBody()).assertNext(getData -> {
-
-      assertThat(getData, is(notNullValue()));
-
-    }).expectComplete().verify();
-
-  }
 
   @Test
   @Disabled
@@ -154,7 +65,6 @@ public class UserIT {
           }).expectComplete().verify();
 
         }).expectComplete().verify();
-
 
   }
 
