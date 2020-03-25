39434a2425ce3f7659457315c2e3374c5fc16086
Jonathan Jogenfors <etnoy@broach.se>
2020-03-25 15:41:18 +0100

    Fix invalid string literal

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
 @Component({
-  selector: 'app-home",
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
