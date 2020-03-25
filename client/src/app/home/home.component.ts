bd27333324adcf774c7a76a3903801f613ade9a0
Jonathan Jogenfors <etnoy@broach.se>
2020-03-25 15:24:27 +0100

    Fix lint issues on home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,22 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home",
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   users = [];
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
-
-    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+    this.dataService.sendGetRequest().subscribe((data: any[]) => {
       console.log(data);
       this.users = data;
-    })
+    });
   }
-
 }
