888482f822b9ff50a7372c2cb3d6399f4f111044
Jonathan Jogenfors <etnoy@broach.se>
2020-04-09 10:00:18 +0200

    Remove constructor in module

--- server/src/main/java/org/owasp/securityshepherd/model/Module.java
+++ server/src/main/java/org/owasp/securityshepherd/model/Module.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 import lombok.With;
 
 @Value
+@AllArgsConstructor
 @Builder
 @With
 public class Module implements Serializable {
@@ -32,21 +34,5 @@ public class Module implements Serializable {
   private String flag;
 
   private boolean isOpen;
-  
-  @java.beans.ConstructorProperties({"id", "name", "description", "isFlagEnabled", "isFlagExact", "flag", "isOpen"})
-  @java.lang.SuppressWarnings("all")
-  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(justification = "generated code")
-  Module(final Long id, @NonNull final String name, final String description, @JsonProperty("isFlagEnabled") final boolean isFlagEnabled, @JsonProperty("isFlagExact") final boolean isFlagExact, final String flag, final boolean isOpen) {
-    if (name == null) {
-      throw new java.lang.NullPointerException("name is marked non-null but is null");
-    }
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.isFlagEnabled = isFlagEnabled;
-    this.isFlagExact = isFlagExact;
-    this.flag = flag;
-    this.isOpen = isOpen;
-  }
 
 }
