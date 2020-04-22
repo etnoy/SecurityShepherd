5dd6fc0318749a7be586e985694e453bb7c1dd2a
Jonathan Jogenfors <etnoy@broach.se>
2020-04-22 12:00:03 +0200

    Move flag submission endpoint to separate controller

--- server/src/main/java/org/owasp/securityshepherd/module/FlagController.java
+++ server/src/main/java/org/owasp/securityshepherd/module/FlagController.java
@@ -16,63 +16,32 @@
 
 package org.owasp.securityshepherd.module;
 
-import javax.validation.Valid;
-import org.owasp.securityshepherd.dto.SubmissionDto;
 import org.owasp.securityshepherd.model.Submission;
 import org.owasp.securityshepherd.service.SubmissionService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
-public class ModuleController {
+public class FlagController {
   private final SubmissionService submissionService;
 
-  private final ModuleService moduleService;
-  
-  private final ModuleSolutions moduleListingComponent;
-
-  @GetMapping(path = "module/{id}")
+  @PostMapping(path = "flag/submit/{id}")
   @PreAuthorize("hasRole('ROLE_USER')")
-  public Mono<Module> getModuleById(@PathVariable final long id) {
-    return moduleService.findById(id);
-  }
-
-  @GetMapping(path = "module/by-name/{shortName}")
-  @PreAuthorize("hasRole('ROLE_USER')")
-  public Mono<ModuleListItem> getModuleByShortName(@PathVariable final String shortName) {
+  public Mono<Submission> submitFlag(@PathVariable("id") final Long moduleId,
+      @RequestBody final String flag) {
     return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
         .map(Authentication::getPrincipal).cast(Long.class)
-        .flatMap(userId -> moduleListingComponent.findOpenModuleByShortNameWithSolutionStatus(userId, shortName));
-  }
-
-  @GetMapping(path = "modules")
-  @PreAuthorize("hasRole('ROLE_USER')")
-  public Flux<ModuleListItem> findAll() {
-    return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
-        .map(Authentication::getPrincipal).cast(Long.class)
-        .flatMapMany(moduleListingComponent::findOpenModulesByUserIdWithSolutionStatus);
-  }
-
-  @PostMapping(path = "module/")
-  @PreAuthorize("hasRole('ROLE_USER')")
-  public Mono<Boolean> submitById(@RequestBody @Valid SubmissionDto submissionDto) {
-    return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
-        .map(Authentication::getPrincipal).cast(Long.class)
-        .flatMap(userId -> submissionService
-            .submit(userId, submissionDto.getModuleId(), submissionDto.getFlag())
-            .map(Submission::isValid));
+        .flatMap(userId -> submissionService.submit(userId, moduleId, flag));
   }
 }
