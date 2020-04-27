d4b70aa0957de4679a37f4e481227e1c47ee7ad6
Jonathan Jogenfors <etnoy@broach.se>
2020-04-28 00:17:11 +0200

    Refactor sql tutorial controller

--- server/src/main/java/org/owasp/securityshepherd/module/sqlinjection/SqlInjectionTutorialController.java
+++ server/src/main/java/org/owasp/securityshepherd/module/sqlinjection/SqlInjectionTutorialController.java
@@ -16,10 +16,8 @@
 
 package org.owasp.securityshepherd.module.sqlinjection;
 
+import org.owasp.securityshepherd.security.ControllerAuthentication;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,11 +31,12 @@ import reactor.core.publisher.Flux;
 public class SqlInjectionTutorialController {
   private final SqlInjectionTutorial sqlInjectionTutorial;
 
+  private final ControllerAuthentication controllerAuthentication;
+
   @PostMapping(path = "search")
   @PreAuthorize("hasRole('ROLE_USER')")
   public Flux<SqlInjectionTutorialRow> search(@RequestBody final String query) {
-    return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
-        .map(Authentication::getPrincipal).cast(Long.class)
+    return controllerAuthentication.getUserId()
         .flatMapMany(userId -> sqlInjectionTutorial.submitQuery(userId, query));
   }
 }
