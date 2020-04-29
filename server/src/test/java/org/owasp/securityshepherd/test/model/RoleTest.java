65c30dd1e2ecf745e6e5e65490f489074899e08c
Jonathan Jogenfors <etnoy@broach.se>
2020-04-29 10:00:05 +0200

    Role tests

---
+++ server/src/test/java/org/owasp/securityshepherd/test/model/RoleTest.java
@@ -0,0 +1,38 @@
+/**
+ * This file is part of Security Shepherd.
+ *
+ * Security Shepherd is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Security Shepherd is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Security Shepherd.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.owasp.securityshepherd.test.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.owasp.securityshepherd.security.AuthResponse;
+import org.owasp.securityshepherd.security.Role;
+import org.owasp.securityshepherd.test.util.TestUtils;
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+@DisplayName("Role unit test")
+public class RoleTest {
+  @Test
+  public void userRole_ToString_IsRoleUser() {
+    assertThat(Role.ROLE_USER.toString()).isEqualTo("ROLE_USER");
+  }
+
+  @Test
+  public void adminRole_ToString_IsRoleAdmin() {
+    assertThat(Role.ROLE_ADMIN.toString()).isEqualTo("ROLE_ADMIN");
+  }
+}
