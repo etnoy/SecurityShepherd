c599ff6d5b38f0c335302c9be9c287332747cab6
Jonathan Jogenfors <etnoy@broach.se>
2020-04-29 09:56:15 +0200

    AuthResponse unit test

--- server/src/main/java/org/owasp/securityshepherd/security/AuthResponse.java
+++ server/src/main/java/org/owasp/securityshepherd/security/AuthResponse.java
@@ -17,9 +17,11 @@
 package org.owasp.securityshepherd.security;
 
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
+@AllArgsConstructor
 public final class AuthResponse implements Serializable {
   private static final long serialVersionUID = 5631647781132001719L;
   
