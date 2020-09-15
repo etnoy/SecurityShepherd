b08a9d9b3d9117b2768de6c88a748c2b171cd23b
Jonathan Jogenfors <etnoy@broach.se>
2020-09-15 23:39:41 +0200

    Add csrf tutorial controller

---
+++ server/src/main/java/org/owasp/securityshepherd/module/csrf/CsrfTutorialController.java
@@ -0,0 +1,42 @@
+/**
+ * This file is part of Security Shepherd.
+ *
+ * Security Shepherd is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Security Shepherd is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Security Shepherd.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.owasp.securityshepherd.module.csrf;
+
+import javax.validation.constraints.Min;
+import org.owasp.securityshepherd.authentication.ControllerAuthentication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/module/csrf-tutorial")
+public class CsrfTutorialController {
+
+  private final ControllerAuthentication controllerAuthentication;
+
+  @GetMapping(path = "csrf/{userId}")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  public Mono<Long> csrfTarget(@Min(1) @PathVariable final long userId) {
+    return controllerAuthentication.getUserId(); // TODO: csrf handling
+  }
+
+}
